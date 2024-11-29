3f1ce7e3c2dce812a98ccc2b7c755a43649b7010
KevinLin <dolphin0317@gmail.com>
2024-11-29 17:47:19 +0800

    Update replace-html.sh

--- replace-html.sh
+++ replace-html.sh
@@ -9,12 +9,16 @@ index_file="index_sample"
 file_list="file-list.txt"
 output_file="index.html"
 
+# 獲取當前時間，格式為 yyyy-MM-dd HH:mm:ss
+current_time=$(date "+%Y-%m-%d %H:%M:%S")
+
 # 使用 sed 替換內容
 sed "/<!-- file-list.txt 的內容將自動插入至此處 -->/ {
     r ${file_list}
     d
-}" "${index_file}" > "${output_file}"
+}
+s/<!-- LastRenewTime -->/${current_time}/" "${index_file}" > "${output_file}"
 
 echo "已生成更新後的檔案：${output_file}"
 
-rm -f ./${file_list}
+rm -f ./${index_file} ./${file_list} 
