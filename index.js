5147681de084295f632eb2ef577d2b5b77c220b1
KevinLin <dolphin0317@gmail.com>
2025-02-12 13:10:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-
+// 文章資料
+const articles = [
+    "20250207_103719_使用AWS CloudFront 的原始存取控制(OAC)建立安全的 S3 靜態網站 ｜ by Kuro Huang ｜ 資安工作者的學習之路 ｜ Medium.html",
+    "20250207_100514_AWS WAF 設定, 無法上傳檔案的問題 (403 Forbidden error) ： Minibox.html",
+    "20250207_100409_AWS S3 公開存取權完整指南.html",
+    "20250124_162548_S3に保存したAWS WAFログをAthenaで分析してみた ｜ DevelopersIO.html",
+    "20250124_154333_使用Athena分析在S3上的WAF Log – PCMAN的技術博客 – 用看得懂的方式講AWS！.html",
+    "20250122_171931_SSM權限設定問題.html",
+    "20250122_122308_DNS ｜ * 記錄 ｜ kl317.us.kg ｜ [email protected]'s Account ｜ Cloudflare.html",
+    "20250109_175546_將 Microsoft AD 與介面類型 VPC 終端節點組合在一起時要設置的內容 ｜ 開發者IO.html",
+    "20250108_094019_為 AWS CLI 安裝 Session Manager 外掛程式 - AWS Systems Manager.html",
+    "20241206_163057_EC2 系統日誌分析.html",
+    "20241205_002043_AWS ECR 建構多平台 Images 的操作過程.html",
+    "20241202_123702_使用 WindowsDNS 的 ConditionalForwarders(條件式轉寄站) 加上 AmazonProvidedDNS.html",
+    "20241127_164654_自動發佈到 GitHub Pages.html",
+    "20241123_175255_管理个人访问令牌 - GitHub 文档.html",
+    "20241123_174801_使用 GitHub 免費製作個人網站 - 為你自己學 Git ｜ 高見龍.html",
+    "20241123_174547_從 0 到 1 的 GitHub Pages 教學手冊 - HackMD.html",
+    "20241123_174308_Vue - 使用 GitHub Actions 部署到 GitHub Pages - 想飛的葉子.html",
+    "20241123_174303_使用 GitHub Actions 进行部署 - GitHub 文档.html",
+    "20241123_174236_何謂 CI_CD ？ 利用 Github Actions 做一個簡單的 CI_CD ｜ by Sean Chou ｜ Recording everything ｜ Medium.html",
+    "20241123_174221_CI_CD： GitHub Actions 自動部署到 GitHub Page.html",
+    "20241123_174001_使用 GitHub Actions 來自動部署 GitHub Pages - iT 邦幫忙：一起幫忙解決難題，拯救 IT 人的一天.html"
+];
 
 // 分頁設定
 const itemsPerPage = 8;
